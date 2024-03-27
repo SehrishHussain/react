9643550504075e1625858966d6963a41fccdf8c9
SehrishHussain <sehrishaslam1@hotmail.com>
2024-03-27 00:25:57 -0700

    currency converter

---
+++ currencyConverter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-50'>Currency app</h1>
+    </>
+  )
+}
+
+export default App
---
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+
+function useCurrencyInfo(currency) {
+    cosnt [data, setData] = useState({}); // emptry array incase comes prog should not crash, thats why empty {} given
+    useEffect(() => {
+        fetch(`https://currency-api.pages.dev/v1/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        console.log(data);
+    }, [currency])
+    return data
+
+
+}
+
+export default useCurrencyInfo;
