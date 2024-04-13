4e7f78343d66d2f729c7c4d0b6e1d9951e22f55c
SehrishHussain <sehrishaslam1@hotmail.com>
2024-04-13 06:49:34 -0700

    new tic tac toe

---
+++ tic-tac-toe/src/components/Square.jsx
@@ -0,0 +1,16 @@
+//import { useState } from "react";
+
+function Square({value, onSquareClick}){
+   // const [value, setValue] = useState(null);
+
+    
+    return (
+         <button
+            className="square" 
+            onClick={onSquareClick}
+         >
+            {value}
+        </button>
+    );
+}
+export default Square;
