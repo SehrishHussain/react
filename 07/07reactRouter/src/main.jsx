7f0094dcc5a8ddba7cb493c35aef2a6c136ccb0f
SehrishHussain <sehrishaslam1@hotmail.com>
2024-05-25 08:57:48 -0700

    07reactRouterWorking

--- 07/07reactRouter/src/main.jsx
+++ 07/07reactRouter/src/main.jsx
@@ -6,9 +6,11 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
-import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+import Github from './components/Github/Github.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -17,15 +19,15 @@ import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 //     children: [
 //       {
 //         path: "",
-//         element: <Home />
+//         element: <Home/>
 //       },
 //       {
-//         path: "about",
-//         element: <About />
+//         path: 'about',
+//         element: <About/>
 //       },
 //       {
-//         path: "contact",
-//         element: <Contact />
+//         path: 'contact',
+//         element: <Contact/>
 //       }
 //     ]
 //   }
@@ -33,16 +35,15 @@ import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='contact' element={<Contact />} />
-      <Route path='user/:userid' element={<User />} />
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
+      <Route path='user/:userid' element={<User/>}/>
       <Route 
-      loader={githubInfoLoader}
-      path='github' 
-      element={<Github />}
-       />
+      
+      path='github' element={<Github/>}/>
+
     </Route>
   )
 )
