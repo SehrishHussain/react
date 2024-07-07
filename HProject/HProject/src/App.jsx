e81f2816b6e37673d76ab5c28a9f044289c9fd2c
SehrishHussain <sehrishaslam1@hotmail.com>
2024-07-07 01:45:44 -0700

    confg.js, Redux store setup

--- HProject/HProject/src/App.jsx
+++ HProject/HProject/src/App.jsx
@@ -1,15 +1,42 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import authService from "./appwrite/auth"
 import './App.css'
+import { useDispatch } from 'react-redux'
+import {login, logout} from "./store/authSlice"
+import { Header } from './components'
+import { Footer } from './components'
+import conf from './conf/conf'
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log("ProjectID:",conf.appwriteProjectId)
+ const [loading, setLoading] = useState(true)
+const dispatch = useDispatch();
+
+useEffect(() => {
+  authService.getCurrentUser()
+  .then((userData) => {
+    if (userData) {
+      dispatch(login({userData}))
+    } else {
+      dispatch(logout())
+    }
+  })
+  .finally(() => setLoading(false))
+
+}, [])
 
   return (
-    <> A project with appwrite
-    {console.log(import.meta.env.VITE_APPWRITE_URL)}
-     
-    </>
+    !loading ? (
+      <div className='min-h-screen flex flex-wrap content-between bg-red-400'>
+        <div className='w-full block'>
+          <Header/>
+          <main>
+
+          </main>
+          <Footer/>
+        </div>
+      </div>
+    ) : null
   )
 }
 
--- HProject/HProject/src/conf/conf.js
+++ HProject/HProject/src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf = {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
-    appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
-    appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
-    appwriteBucketId: String(import.meta.env.VITE_BUCKET_ID)
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID), // Fixed typo here
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID), // Fixed typo here
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID), // Fixed typo here
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID) // Fixed typo here
 }
-
-export default conf
+console.log("Appwrite Configuration:", conf);
+export default conf;
