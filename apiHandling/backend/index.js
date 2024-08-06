6578932e3c1393d0a9c0192093b1401e2b0193bb
SehrishHussain <sehrishaslam1@hotmail.com>
2024-08-05 23:51:58 -0700

    DONE APIS

--- apiHandling/backend/index.js
+++ apiHandling/backend/index.js
@@ -7,11 +7,11 @@ app.use(cors());
 app.get('/api/products', (req, res) => {
     const products = [
         { id: 1, name: 'Item1', price: 10, image: 'https://images.pexels.com/photos/1' },
-        { id: 2, name: 'Item2', price: 20, image: 'https://images.pexels.com/photos/2' },
+        { id: 2, name: 'wood', price: 20, image: 'https://images.pexels.com/photos/2' },
         { id: 3, name: 'Item3', price: 30, image: 'https://images.pexels.com/photos/3' },
-        { id: 4, name: 'Item4', price: 40, image: 'https://images.pexels.com/photos/4' },
-        { id: 5, name: 'Item5', price: 50, image: 'https://images.pexels.com/photos/5' },
-        { id: 6, name: 'Item6', price: 60, image: 'https://images.pexels.com/photos/6' },
+        { id: 4, name: 'wood', price: 40, image: 'https://images.pexels.com/photos/4' },
+        { id: 5, name: 'metal', price: 50, image: 'https://images.pexels.com/photos/5' },
+        { id: 6, name: 'plastic', price: 60, image: 'https://images.pexels.com/photos/6' },
         { id: 7, name: 'Item7', price: 70, image: 'https://images.pexels.com/photos/7' },
         { id: 8, name: 'Item8', price: 80, image: 'https://images.pexels.com/photos/8' },
         { id: 9, name: 'Item9', price: 90, image: 'https://images.pexels.com/photos/9' },
--- apiHandling/frontend/src/App.jsx
+++ apiHandling/frontend/src/App.jsx
@@ -5,18 +5,73 @@ import './App.css'
 
 function App() {
 
-  const [products, error, loading] = customReactQuery('/api/products')
+ // const [products, error, loading] = customReactQuery('/api/products')
   
-  if (error) {
-    return <h1>Something went wrong</h1>  //if error this code will run o.w return code
-  }
+ const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState('');
+  
+  useEffect (() => {
+    //nextline is for reducing calling search fn everytime keyboard is typed.
+    //and its part of powerful axios library
+    const controller = new AbortController() //we have to send this controller with axios.get 
+   //FIRST STEP: make controller
+   
+    //const prodctsstore = await axios.get('/api/products').then((res) => {})
+    // we cant do it here cz we need to use async for await n cant use async in useEffect def so..We use Iffe (immediately invoked fns
+    // ()() one parenthesis has fn def, one has fn call. They r called where declared)
+    ;(async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get('/api/products?search=' + search, {
+          signal: controller.signal //SECOND STEP: send its signal
+        })
+        
+        console.log("RESPonse: ",response.data);
+        setProducts(response.data)
+        setLoading(false);
+      } catch (error) {
+        //THID STEP: axios k request jo cancle he hai usko handle krna
+        if (axios.isCancel(error)){
+          console.log('Request cancelled', error.message)
+          return //nothing return just exit
+        }
+        setError(true);
+        setLoading(false);
+        
+      }
+    })()
 
-  if (loading) {
-    return <h1>Loading</h1>
-  }
-  return (
+    //FORTH STEP: Every useEffect has return its called CLEANUP METHOD
+    //the event handler is unmount here. Here components are unmounted. e.g: eventhandler on
+   //i.e. the cleanup code
+    return () => {
+      controller.abort() // i.e comp unmount
+    }
+  },[search])
+
+ 
+
+  // if (error) {
+  //   return <h1>Something went wrong</h1>  //if error this code will run o.w return code
+  // }
+
+  // if (loading) {
+  //   return <h1>Loading</h1>
+  // }
+  return ( //conditional rendering instead of returnS
     <>
       <h1>Chai aur API in React</h1>
+
+      <input type="text" placeholder='Search'
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+      />
+
+      {loading && (<h1>Loading...</h1>)} 
+      {error && (<h1>Somthing went wrong</h1>)}
+
       <h2>Number of Products are: {products.length}</h2>
     </>
   )
@@ -25,30 +80,3 @@ function App() {
 export default App
 
 // Some ppl take all the code and make a custom hook like given below
-const customReactQuery = (urlPath) => {
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  
-  useEffect (() => {
-   //const prodctsstore = await axios.get('/api/products').then((res) => {})
-    // we cant do it here cz we need to use async for await n cant use async in useEffect def so..We use Iffe (immediately invoked fns
-    // ()() one parenthesis has fn def, one has fn call. They r called where declared)
-    (async () => {
-      try {
-        setLoading(true);
-        const response = await axios.get(urlPath)
-        console.log("RESPonse: ",response.data);
-        setProducts(response.data)
-        setLoading(false);
-      } catch (error) {
-        setError(true);
-        setLoading(false);
-        
-      }
-    })()
-  },[])
-
- return [products, error, loading];
-
-}
